34f001cc2bad6f14fb796a1aabaa88be92b1efda
David del Real <warhead_1090@hotmail.com>
2020-12-18 17:46:23 -0600

    part 6 tutorial, hypelinking stuff in api

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -10,7 +10,24 @@ from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.reverse
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response({
+        "users": reverse("user-list", request=request, format=format),
+        "snippets": reverse("snippet-list", request=request, format=format)
+    })
+
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
